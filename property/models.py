cfdff5fed951b42b04588ef939f8c89f5d4d2332
Akin Kok <aakok@aakok.local>
2012-09-09 22:18:51 +0300

    some data changes and interest calculation introduced.

--- property/models.py
+++ property/models.py
@@ -28,6 +28,18 @@ class Property(models.Model):
     contributors = models.ManyToManyField(
         'auth.User', related_name='contributionproperty_set')
 
+    @staticmethod
+    def interest_calculation(price, months, monthly_interest):
+        return (price * (monthly_interest / 100.0)) / (
+            1 - (1 / ((1 + (monthly_interest / 100.0)) ** months)))
+
+    def garanti_1_25_60(self):
+        return {
+            '_75': Property.interest_calculation((self.price * .75), 60, 1.25),
+            '_25_24': Property.interest_calculation((self.price * .25), 24, 1.25),
+            '_25_60': Property.interest_calculation((self.price * .25), 60, 1.25),
+            }
+
 
 class PropertyComment(models.Model):
     user = models.ForeignKey('auth.user')
